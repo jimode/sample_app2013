333bf0028067f64f4e1c62f5bf23203729df1095
Jimi Odeyemi <jimode68@gmail.com>
2013-03-07 21:12:20 +0000

    Finish static pages with additional exercises

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,7 +4,7 @@ describe "Static Pages" do
 
 	describe "Home page" do
 
-		it "should have the content 'Sample App'" do
+		it "should have the h1 'Sample App'" do
 			visit '/static_pages/home'
 			page.should have_selector('h1', :text => 'Sample App')
 		end
@@ -18,7 +18,7 @@ describe "Static Pages" do
 
 	describe "Help page" do
 
-		it "should have the content 'Help'" do
+		it "should have the h1 'Help'" do
 			visit '/static_pages/help'
 			page.should have_selector('h1', :text => 'Help')
 		end
@@ -33,17 +33,31 @@ describe "Static Pages" do
 
 	describe "About page" do
 
-		it "should have the content 'About us'" do
+		it "should have the h1 'About us'" do
 			visit '/static_pages/about'
 			page.should have_selector('h1', :text => 'About Us')
 		end
 
 		it "should have the right title" do
 		  visit '/static_pages/about'
-		  page.should	have_selector('title', 
+		  page.should have_selector('title', 
 	                          :text => "Ruby on Rails Tutorial Sample App | About Us")
 		end
 		
+	end
+
+	describe "Contact page" do
+
+		it "should have the h1 'Contact'" do
+			visit '/static_pages/contact'
+			page.should have_selector('h1', :text => 'Contact')
+		end
+		
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			page.should have_selector('title',
+							:text => "Ruby on Rails Tutorial Sample App | Contact")	
+		end
 	end
 
 end
